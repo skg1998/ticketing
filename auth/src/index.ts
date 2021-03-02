94e9cbd8e5c360acca9ba53d1ef7dbe60f7b9c59
skg1998 <sahilkg1998@gmail.com>
2021-03-02 17:57:28 +0530

    add cookie-session

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
+
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
@@ -10,8 +12,15 @@ import { errorHandler } from './middleware/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+app.set('trust proxy', true);
 const port = 3000;
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -33,7 +42,7 @@ const start = async () => {
     });
     console.log('mongodb is connected sucessful');
   } catch (err) {
-    console.log(`Their is some err : ${err}`);
+    console.log(`Their is some err: ${err}`);
   }
 
   app.listen(port, () => {

cfdee12819f5b1bf192e65ba22717ecdc7d64c63
skg1998 <sahilkg1998@gmail.com>
2021-03-22 18:42:20 +0530

    update

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,14 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@ticketing-pro/common';
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from '@ticketing-pro/common';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes';
 
 const app = express();
 app.set('trust proxy', true);
@@ -13,6 +20,11 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+
+app.use(currentUser);
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();

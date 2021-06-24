033cae276bb742cbe908f3b4bca5db6a264eeb99
skg1998 <sahilkg1998@gmail.com>
2021-06-24 14:54:15 +0530

    feat(nats-streaming): create a nats-streaming for event management

---
+++ nats-test/src/events/base-listener.ts
@@ -0,0 +1,49 @@
+import { Message, Stan } from 'node-nats-streaming';
+import { Subjects } from './subjects';
+
+interface Event {
+  subject: Subjects;
+  data: any;
+}
+
+export abstract class Listener<T extends Event> {
+  abstract subject: T['subject'];
+  abstract queueGroupName: string;
+  abstract onMessage(data: T['data'], msg: Message): void;
+  private cient: Stan;
+  protected ackWait = 5 * 1000;
+
+  constructor(client: Stan) {
+    this.cient = client;
+  }
+
+  subscriptionOptions() {
+    return this.cient
+      .subscriptionOptions()
+      .setDeliverAllAvailable()
+      .setManualAckMode(true)
+      .setAckWait(this.ackWait)
+      .setDurableName(this.queueGroupName);
+  }
+
+  listen() {
+    const subscription = this.cient.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    );
+
+    subscription.on('message', (msg: Message) => {
+      console.log(`Message Recevied: ${this.subject}/${this.queueGroupName}`);
+      const parseData = this.parseMessage(msg);
+      this.onMessage(parseData, msg);
+    });
+  }
+
+  parseMessage(msg: Message) {
+    const data = msg.getData();
+    return typeof data === 'string'
+      ? JSON.parse(data)
+      : JSON.parse(data.toString('utf8'));
+  }
+}

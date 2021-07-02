f7c08c5cd487de3b3feaa814427c1bab56340f95
skg1998 <sahilkg1998@gmail.com>
2021-07-02 15:41:34 +0530

    feat(orderschema): create a order schema

---
+++ orders/src/models/order.models.ts
@@ -0,0 +1,59 @@
+import mongoose from 'mongoose';
+import { OrderStatus } from '@ticketing-pro/common';
+import { TicketDoc } from './ticket';
+
+interface OrderAttrs {
+  userId: string;
+  status: OrderStatus;
+  expiresAt: string;
+  ticket: TicketDoc;
+}
+
+interface OrderDoc extends mongoose.Document {
+  userId: string;
+  status: OrderStatus;
+  expiresAt: string;
+  ticket: TicketDoc;
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attr: OrderAttrs): OrderDoc;
+}
+
+const OrderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(OrderStatus),
+      default: OrderStatus.Created,
+    },
+    expiresAt: {
+      type: mongoose.Schema.Types.Date,
+    },
+    ticket: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Ticket',
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+OrderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order(attrs);
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>('Order', OrderSchema);
+
+export { Order };

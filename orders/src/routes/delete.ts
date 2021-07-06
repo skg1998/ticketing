54cc45d10e9c72b2c9e59f1d34da9ab8bd520ab2
skg1998 <sahilkg1998@gmail.com>
2021-07-06 16:29:22 +0530

    feat(order): create show & delete api

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,26 @@
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  OrderStatus,
+} from '@ticketing-pro/common';
 import express, { Request, Response } from 'express';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
 router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
+  const order = await Order.findById(req.params.orderId);
+
+  if (!order) {
+    throw new NotFoundError();
+  }
+
+  if (order.userId !== req.currentUser!.id) {
+    throw new NotAuthorizedError();
+  }
+  order.status = OrderStatus.Cancelled;
+  await order.save();
+  res.status(204).send(order);
 });
 
 export { router as deleteOrderRouter };

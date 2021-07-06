0210b456c03941971740052eed782758fd585d84
skg1998 <sahilkg1998@gmail.com>
2021-07-06 16:27:58 +0530

    feat(test): create test for orders

---
+++ orders/src/routes/__test__/index.test.ts
@@ -0,0 +1,53 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Order } from '../../models/order';
+import { Ticket } from '../../models/ticket';
+
+const buildTicket = async () => {
+  const ticket = await Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+  return ticket;
+};
+
+it('fetches order for a particular user', async () => {
+  // Create three Ticket
+  const ticketone = await buildTicket();
+  const tickettwo = await buildTicket();
+  const ticketthree = await buildTicket();
+
+  const userOne = global.signin();
+  const userTwo = global.signin();
+
+  // Create one order as User #1
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', userOne)
+    .send({ ticketId: ticketone.id })
+    .expect(201);
+
+  // Create two order as user #2
+  const { body: orderOne } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', userTwo)
+    .send({ ticketId: tickettwo.id })
+    .expect(201);
+
+  const { body: orderTwo } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', userTwo)
+    .send({ ticketId: ticketthree.id })
+    .expect(201);
+
+  // Make request to get orders for users #2
+  const response = await request(app).get('/api/orders').set('Cookie', userTwo);
+
+  // Make sure to only got the orders for users #2
+  expect(response.body.length).toEqual(2);
+  expect(response.body[0].id).toEqual(orderOne.id);
+  expect(response.body[1].id).toEqual(orderTwo.id);
+  expect(response.body[0].ticket.id).toEqual(tickettwo.id);
+  expect(response.body[1].ticket.id).toEqual(ticketthree.id);
+});
---
+++ orders/src/routes/__test__/show.test.ts
@@ -0,0 +1,55 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+
+it('fetches the order', async () => {
+  // Create a ticket
+  const ticket = await Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signin();
+
+  // make a request to build an order with this ticket
+  const { body: order } = await request(app)
+    .get('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // make request to fetch the ticket
+  const { body: fetchedOrder } = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(200);
+
+  expect(fetchedOrder.id).toEqual(order.id);
+});
+
+it('return an error if one user is try to fetch other user data', async () => {
+  // Create a ticket
+  const ticket = await Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signin();
+
+  // make a request to build an order with this ticket
+  const { body: order } = await request(app)
+    .get('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // make request to fetch the ticket
+  await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', global.signin())
+    .send()
+    .expect(401);
+});

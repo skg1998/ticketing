0ebe7d1087091a57ffb422ddf3b118296aafc4a5
skg1998 <sahilkg1998@gmail.com>
2021-07-17 19:47:27 +0530

    feat(version): update version in order

--- orders/src/events/listeners/ticket-updated-listeners.ts
+++ orders/src/events/listeners/ticket-updated-listeners.ts
@@ -8,7 +8,7 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvents> {
   queueGroupName = queueGroupName;
 
   async onMessage(data: TicketUpdatedEvents['data'], msg: Message) {
-    const ticket = await Ticket.findById(data.id);
+    const ticket = await Ticket.findByEvent(data);
 
     if (!ticket) {
       throw new Error('Ticket not found');
@@ -16,7 +16,6 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvents> {
 
     const { title, price } = data;
     ticket.set({ title, price });
-
     await ticket.save();
 
     msg.ack();

c6d0876a52448691edeffbd80cd09357ae5fd132
skg1998 <sahilkg1998@gmail.com>
2021-07-21 14:01:28 +0530

    feat(expiration service): create expiration serivce to handle cancel ticket after a expires ticket

---
+++ expiration/src/__mocks__/nats-wrapper.ts
@@ -0,0 +1,11 @@
+export const natsWrapper: any = {
+  client: {
+    publish: jest
+      .fn()
+      .mockImplementation(
+        (subject: string, data: string, callback: () => void) => {
+          callback();
+        }
+      ),
+  },
+};

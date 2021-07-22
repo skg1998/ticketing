ff6d6d3afe12ef71389b8aad003ef342fd47e1ac
skg1998 <sahilkg1998@gmail.com>
2021-07-22 13:48:50 +0530

    feat(listener): create expiration complete listener

---
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,42 @@
+import {
+  Listener,
+  ExpirationCompleteEvent,
+  Subjects,
+  OrderStatus,
+} from '@ticketing-pro/common';
+import { queueGroupName } from './queue-group-name';
+import { Order } from '../../models/order';
+import { Message } from 'node-nats-streaming';
+import { OrderCancelledPublisher } from '../publishers/order-cancelled-publisher';
+import { natsWrapper } from '../../nats-wrapper';
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+  subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
+    const order = await Order.findById(data.orderId);
+
+    if (!order) {
+      throw new Error('Order not found');
+    }
+
+    if (order.status === OrderStatus.Complete) {
+      return msg.ack();
+    }
+
+    order.set({
+      status: OrderStatus.Cancelled,
+    });
+
+    await order.save();
+
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
+  }
+}
